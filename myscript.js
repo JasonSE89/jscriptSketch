43633dce98600b9174618a0be95a24ef6edfbe76
Jason <jason.epstein89@gmail.com>
2017-01-26 16:53:17 -0500

    Basic project completed

--- myscript.js
+++ myscript.js
@@ -1,14 +1,37 @@
 $(document).ready(function(){
+
+
 	 $("td").mouseenter(function(){
 	 		$(this).css("background-color", "black");
 			}
 
 	 	);
-	$("#clear").on({
-		click: function(){
+	$("#clear").on(
+	{
+		click: function()
+		{
 			$("td").css("background-color", "white");
-		}
-	}
-	);
+		
+	
+}
 }
 );
+}
+);
+
+function dimensions(){
+	var dim = prompt("What do you want the dimensions to be");
+	$("table").remove();
+	$("body").append("<table></table>");
+	for (var i = dim; i > 0; i--) {
+			$("table").append("<tr></tr>");
+		}
+	for(var a = dim; a > 0; a--){
+			$("tr").append("<td></td>");
+			}
+	$("td").mouseenter(function(){
+	 		$(this).css("background-color", "black");
+			}
+
+	 	);
+}
