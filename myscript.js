124fe54b8a80fb021cfbeb900aa51015d32b0535
Jason <jason.epstein89@gmail.com>
2017-01-26 12:52:01 -0500

    Clear button works

--- myscript.js
+++ myscript.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
-	$("td").mouseenter(function(){
-			$(this).css("background-color", "black");
+	 $("td").mouseenter(function(){
+	 		$(this).css("background-color", "black");
 			}
 
-		);
+	 	);
+	$("#clear").on({
+		click: function(){
+			$("td").css("background-color", "white");
+		}
+	}
+	);
 }
 );
